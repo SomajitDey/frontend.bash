7754558bec94c7a71f63bf0c25ca0d5b29a990be
Somajit <dey.somajit@gmail.com>
2021-04-13 00:36:18 +0530

    TODO: arrow and Pgup/dn keys for scroll using fold

--- ui.bash
+++ ui.bash
@@ -12,17 +12,39 @@
   # Use inotify (requires installation of inotify-tools) or use the rather resource-hungry hence stupid
   # while sleep 1; do currhash="$(sha256sum LICENSE | cut -d ' ' -f 1)"; [[ $currhash != $oldhash ]] && date && oldhash=$currhash; done
   
-  viewport=/tmp/viewport
-  tmpfile=/tmp/input
-  pidfile=/tmp/pid
-  echo $$ > $pidfile
-  f(){ tail -qF $tmpfile 2>/dev/null & killme=$!;} 2>/dev/null
-  trap '(kill -9 $killme); tput reset; cat $viewport; echo -e \\n; f' USR1
+  export viewport=/tmp/viewport
+  export tmpfile=/tmp/input
+  export pidfile=/tmp/pid
+
+  prompt(){
+    ( echo $BASHPID > $pidfile
+      local chkptfile=/tmp/chkpt; touch $chkptfile
+      until [[ "$viewport" -nt "$chkptfile" ]]; do sleep 0.1; done
+      handler &
+    )&
+    tail --quiet --f=name --pid=$! $tmpfile 2>/dev/null &
+  } 2>/dev/null
+
+  readline(){
+    while read -re 2>$tmpfile; do
+      [[ -n "$REPLY" ]] && echo "$REPLY" >> $viewport
+    done
+  }
+
+  handler(){
+    tput clear
+    cat $viewport
+    echo -e \\n
+    echo -n "Type here: " 
+    prompt
+  }
+  
+  rm -f $tmpfile
+  trap 'pkill -KILL --parent $(cat $pidfile); pkill -KILL --pidfile ${pidfile}; tput rmcup' exit
   tput smcup
-  trap '(kill -9 $killme) 2>/dev/null; tput rmcup' exit
-  kill -USR1 $$
-  while read -re -p "Type here: " 2>$tmpfile; do
-    echo "$REPLY" >> $viewport && kill -USR1 $$
-  done
+  handler
+  
+  readline
+
   exit
     
