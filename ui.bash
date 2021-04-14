654baec511a4ca130912799c65e2015fa1941c7d
Somajit <dey.somajit@gmail.com>
2021-04-14 14:09:25 +0530

    Reformatted

--- ui.bash
+++ ui.bash
@@ -2,14 +2,13 @@
   # A basic UI terminal that does the following:
   # Display a viewport file (a regular file or sym-link) that is updated by other process(es)
   # Give user an input prompt and echo everything the user types there
-  # Commit the user-input to an inputlog (which can be a regular file, sym-link or pipe)
+  # Output the user-input
   
   # TODO: fold -w $WIDTH -s $viewport or fmt for line-wrapping and reformatting the viewport
   # TODO: tail and head or awk to print logical lines
   
   export viewport="/tmp/viewport.txt"
-  export inputlog="/tmp/input.log"
-  export input_buffer="/tmp/input.tmp"
+  export readline_buffer="/tmp/readline_buffer.tmp"
   export this_pid="$$"
 
   launch_bg(){
@@ -23,25 +22,23 @@
     )&
     
     # Launch process to echo user-input and the readline prompt
-    tail --quiet --f=name --pid="$!" "${input_buffer}" 2>/dev/null &
+    tail --quiet --f=name --pid="$!" "${readline_buffer}" >/dev/tty 2>/dev/null &
   } 2>/dev/null
 
   readline(){
-    while read -re 2> "${input_buffer}"; do
-      rm -f "${input_buffer}"; touch "${viewport}" # So that there is new prompt immediately
-      [[ -n "${REPLY}" ]] && echo "${REPLY}" >> "${inputlog}"
+    while read -re -p"Type here: " 2>"${readline_buffer}"; do
+      rm -f "${readline_buffer}"; touch "${viewport}" # So that there is new prompt immediately
+      echo "${REPLY}"
     done
   }
 
   repaint(){
     tput clear
-    cat "${viewport}"
-    echo -e \\n
-    echo -n "Type here: " 
+    cat "${viewport}" <(echo -e \\n)
     launch_bg
-  }
+  } >/dev/tty
   
-  rm -f "${input_buffer}"
+  rm -f "${readline_buffer}"
   trap 'tput rmcup' exit
   tput smcup
   repaint
